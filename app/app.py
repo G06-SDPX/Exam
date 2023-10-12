c35004bbe8c5062598be89d83b8c2be2aa7897a2
BM-Unnop <122899012+Unnop-Thamtiengtham@users.noreply.github.com>
2023-10-12 13:40:11 +0700

    Task 1

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/mul5/<number>', methods=['GET'])
+def calc(number):
+    return str(float(number)*5).replace(".0","")
+
+if __name__ == '__main__':
+    app.run(debug=True)
